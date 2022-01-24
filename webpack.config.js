10a64704ba3ccce6b52521eaf6c2a9774972a31e
Johannes Raggam <thetetet@gmail.com>
2022-01-24 22:40:50 +0100

    maint(webpack): Configure devServer static directory.

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,11 @@ module.exports = async (env, argv) => {
 
     config = patternslib_config(env, argv, config);
     config.output.path = path.resolve(__dirname, "dist/");
-
     config.resolve.alias.redactor = path.resolve(__dirname, "../redactor");
+
+    if (process.env.NODE_ENV === "development") {
+        config.devServer.static.directory = __dirname;
+    }
 
     return config;
 };
